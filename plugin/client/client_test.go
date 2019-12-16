6072e99795ce59c099b616d32bdd18f30f43842a
Becca Petrin <beccapetrin@gmail.com>
2019-12-16 14:25:05 -0800

    add test

--- plugin/client/client_test.go
+++ plugin/client/client_test.go
@@ -137,6 +137,48 @@ func TestUpdatePassword(t *testing.T) {
 	}
 }
 
+// TestUpdateRootPassword mimics the UpdateRootPassword in the SecretsClient.
+// However, this test must be located within this package because when the
+// "client" is instantiated below, the "ldapClient" is being added to an
+// unexported field.
+func TestUpdateRootPassword(t *testing.T) {
+	testPass := "hell0$catz*"
+
+	config := emptyConfig()
+	config.BindDN = "cats"
+	config.BindPassword = "dogs"
+
+	expectedRequest := testSearchRequest()
+	expectedRequest.BaseDN = config.BindDN
+	conn := &ldapifc.FakeLDAPConnection{
+		SearchRequestToExpect: expectedRequest,
+		SearchResultToReturn:  testSearchResult(),
+	}
+
+	expectedPass, err := formatPassword(testPass)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn.ModifyRequestToExpect = &ldap.ModifyRequest{
+		DN: "CN=Jim H.. Jones,OU=Vault,OU=Engineering,DC=example,DC=com",
+	}
+	conn.ModifyRequestToExpect.Replace("unicodePwd", []string{expectedPass})
+	ldapClient := &ldaputil.Client{
+		Logger: hclog.NewNullLogger(),
+		LDAP:   &ldapifc.FakeLDAPClient{conn},
+	}
+
+	client := &Client{ldapClient}
+
+	filters := map[*Field][]string{
+		FieldRegistry.Surname: {"Jones"},
+	}
+
+	if err := client.UpdatePassword(config, config.BindDN, filters, testPass); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func emptyConfig() *ADConf {
 	return &ADConf{
 		ConfigEntry: &ldaputil.ConfigEntry{
