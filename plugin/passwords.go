e54f0480129ec02775296f31bd2b85b20e72235e
Michael Golowka <72365+pcman312@users.noreply.github.com>
2020-06-04 10:50:04 -0600

    Add comment on GeneratePassword

--- plugin/passwords.go
+++ plugin/passwords.go
@@ -19,6 +19,8 @@ type passwordGenerator interface {
 	GeneratePasswordFromPolicy(ctx context.Context, policyName string) (password string, err error)
 }
 
+// GeneratePassword from the password configuration. This will either generate based on a password policy
+// or from the provided formatter. The formatter/length options are deprecated.
 func GeneratePassword(ctx context.Context, passConf passwordConf, generator passwordGenerator) (password string, err error) {
 	err = passConf.validate()
 	if err != nil {
